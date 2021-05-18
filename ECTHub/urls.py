8ba87357325703780fe8017ffd10a214dcbb8aac
Couto <manuelcouto10@ua.pt>
2021-05-18 14:17:50 +0100

    botao de upload

--- ECTHub/urls.py
+++ ECTHub/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from pages.views import home_view,calendar_view, resources_view
 
 urlpatterns = [
@@ -24,3 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('resources/', resources_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -10,4 +11,10 @@ def calendar_view(request, *args, ** kwargs):
     return render(request, "calendar.html",{})
 
 def resources_view(request, *args, ** kwargs):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        print(uploaded_file.name)
+        print(uploaded_file.size)
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name,uploaded_file)
     return render(request,"resources.html",{})
